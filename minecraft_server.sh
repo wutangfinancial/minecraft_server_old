ecb967e43cf93c68f99164caf4c45de839a5502e
wutangfinancial <35402234+wutangfinancial@users.noreply.github.com>
2019-01-27 15:18:36 -0500

    Create minecraft_server.sh

---
+++ minecraft_server.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+msmemmb=512
+mxmemmb=1792
+
+for (( ; ; ))
+do
+        java -server -Xmx${mxmemmb}M -Xms${msmemmb}M -jar minecraft_server.jar nogui
+        echo "Server closed unexpectedly, restarting in 10 seconds..."
+        sleep 10
+done
